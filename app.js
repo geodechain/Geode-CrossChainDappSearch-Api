e9b5dc79bbd002a3e049784cfbf307a9b46c5f67
klsavaj <krunalsavaj3@gmail.com>
2025-07-22 14:40:58 -0400

    Updated App.js File

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var favoritesRouter = require('./routes/favorites'); 
 
 var app = express();
 
@@ -42,6 +43,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/', favoritesRouter); 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
